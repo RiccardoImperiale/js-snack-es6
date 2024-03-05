4f1daa680d98950a9cbc1a74484feaf80fdb7897
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-05 13:39:43 +0100

    add snack 3

---
+++ snack 3/main.js
@@ -0,0 +1,10 @@
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+// Crea un nuovo array con la lista dei mammiferi.
+const mammals = animals.filter(animal => animal.classe === 'mammiferi');
+console.log(mammals);
