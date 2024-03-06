fd5a4c69e5b7607e752b0f8b800d5068ec13e725
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-06 11:46:47 +0100

    simplify code

--- snack-1-b5/main.js
+++ snack-1-b5/main.js
@@ -7,15 +7,13 @@ const zucchine = [
     { varieta: 'Zucca rampicante', peso: 2, lunghezza: 4 },
     { varieta: 'Zucchina marion', peso: 2.5, lunghezza: 4 },
     { varieta: 'Zucchina giuseppa', peso: 4, lunghezza: 73 },
-    { varieta: 'Zucchina rossa', peso: 4, lunghezza: 37 },
+    { varieta: 'Zucchina rossa', peso: 0, lunghezza: 37 },
     { varieta: 'Zucchina verde', peso: 3, lunghezza: 63 },
     { varieta: 'Zucchina ice', peso: 10, lunghezza: 33 },
     { varieta: 'Zucchina gaerg', peso: 23, lunghezza: 3 },
 ]
 // Calcola quanto pesano tutte le zucchine.
 let pesoZucchine = 0;
-zucchine.forEach(zucchina => {
-    pesoZucchine += zucchina.peso;
-});
+zucchine.forEach(zucchina => pesoZucchine += zucchina.peso);
 
 console.log(pesoZucchine);
